3799386bc5c627298ea42a2e5bfcc1b28d9fdd80
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-17 12:20:46 +0100

    Added the projects I've done section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas} from "./components";
 import Academic from "./components/Academic";
 
 const App = () => {
@@ -16,7 +16,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
