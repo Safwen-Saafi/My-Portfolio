4279c4c64c44c83347331b9860fb0029a0a508f1
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-21 15:01:07 +0100

    Prototype for mobile rendering

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,36 +6,41 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   const [loadedTechnologies, setLoadedTechnologies] = useState([]);
-  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    // Function to load technologies one by one
-    const loadNextTechnology = () => {
-      if (index < technologies.length) {
-        setLoadedTechnologies((prev) => [...prev, technologies[index]]);
-        setIndex((prevIndex) => prevIndex + 1);
+    let currentIndex = 0;
+
+    const loadTechnologies = () => {
+      if (currentIndex < technologies.length) {
+        setLoadedTechnologies((prev) => [
+          ...prev,
+          technologies[currentIndex],
+        ]);
+        currentIndex++;
+        // Use requestIdleCallback if available, otherwise fallback to setTimeout
+        if ('requestIdleCallback' in window) {
+          requestIdleCallback(loadTechnologies);
+        } else {
+          setTimeout(loadTechnologies, 200); // Adjust the timeout as needed
+        }
       }
     };
 
-    // Load the next technology every 500ms
-    const interval = setInterval(loadNextTechnology, 500);
-
-    // Clear the interval when all technologies are loaded
-    if (index >= technologies.length) {
-      clearInterval(interval);
-    }
-
-    return () => clearInterval(interval);
-  }, [index]);
+    loadTechnologies();
+  }, []);
 
   return (
     <>
-      <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <h1 className="font-bold flex justify-center text-lg mb-5 text-[#915EFF]">
+        My Tech Stack
+      </h1>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
         {loadedTechnologies.map((technology) => (
-          <div className='w-28 h-28' key={technology.name}>
+          <div className="w-28 h-28" key={technology.name}>
             <BallCanvas icon={technology.icon} />
-            <p className="flex justify-center text-white font-bold">{technology.name}</p>
+            <p className="flex justify-center text-white font-bold">
+              {technology.name}
+            </p>
           </div>
         ))}
       </div>
