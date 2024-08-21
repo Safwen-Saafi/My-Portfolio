cd80e3d20c2ae9e7a3bbf50337df08966815adfa
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-21 15:05:54 +0100

    Prototype for mobile rendering

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,45 +5,32 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  const [loadedTechnologies, setLoadedTechnologies] = useState([]);
-
-  useEffect(() => {
-    let currentIndex = 0;
-
-    const loadTechnologies = () => {
-      if (currentIndex < technologies.length) {
-        setLoadedTechnologies((prev) => [
-          ...prev,
-          technologies[currentIndex],
-        ]);
-        currentIndex++;
-        // Use requestIdleCallback if available, otherwise fallback to setTimeout
-        if ('requestIdleCallback' in window) {
-          requestIdleCallback(loadTechnologies);
-        } else {
-          setTimeout(loadTechnologies, 200); // Adjust the timeout as needed
-        }
-      }
-    };
-
-    loadTechnologies();
-  }, []);
-
+  const isMobile = window.matchMedia("(max-width: 500px)").matches;
+  const Map = (isMobile ,technologies)=>{
+    if(isMobile){
+      return(
+    technologies.slice(0,4).map((technology)=>(
+    <div className='w-28 h-28' key={technology.name}>
+    <BallCanvas icon={technology.icon}/>
+    <p className="flex justify-center text-white font-bold">{technology.name}</p>
+    </div>
+    )))
+    }
+    else{return(
+      technologies.map((technology)=>(
+        <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon}/>
+        <p className="flex justify-center text-white font-bold">{technology.name}</p>
+        </div>
+      )))
+    }
+  }
   return (
     <>
-      <h1 className="font-bold flex justify-center text-lg mb-5 text-[#915EFF]">
-        My Tech Stack
-      </h1>
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {loadedTechnologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-            <p className="flex justify-center text-white font-bold">
-              {technology.name}
-            </p>
-          </div>
-        ))}
-      </div>
+    <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {Map(isMobile ,technologies)}
+    </div>
     </>
   );
 };
