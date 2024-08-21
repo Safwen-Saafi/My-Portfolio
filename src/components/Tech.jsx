77c42fcbc2b885e2bb071932202b4587160cc4a9
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-21 15:16:24 +0100

    I think final version

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,37 +6,32 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
-  const Map = (isMobile, technologies) => {
-    if (isMobile) {
-      return technologies.slice(0, 4).map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="flex justify-center text-white font-bold">
-            {technology.name}
-          </p>
-        </div>
-      ));
-    } else {
-      return technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="flex justify-center text-white font-bold">
-            {technology.name}
-          </p>
-        </div>
-      ));
+  const Map = (isMobile ,technologies)=>{
+    if(isMobile){
+      return(
+    technologies.slice(0,4).map((technology)=>(
+    <div className='w-28 h-28' key={technology.name}>
+    <BallCanvas icon={technology.icon}/>
+    <p className="flex justify-center text-white font-bold">{technology.name}</p>
+    </div>
+    )))
     }
-  };
+    else{return(
+      technologies.map((technology)=>(
+        <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon}/>
+        <p className="flex justify-center text-white font-bold">{technology.name}</p>
+        </div>
+      )))
+    }
+  }
   return (
     <>
-      <h1 className="font-bold flex justify-center text-lg mb-5 text-[#915EFF]">
-        My Tech Stack
-      </h1>
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {Map(isMobile, technologies)}
-      </div>
+    <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {Map(isMobile ,technologies)}
+    </div>
     </>
   );
 };
-
 export default SectionWrapper(Tech, "");
